44399eca3655616270e09e63660e5fd6541ff4d7
сергей оразаев <cyperman@mail.ru>
2020-03-23 19:29:58 +0300

    upload lab 2.1

---
+++ lab2/printHello/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace printHello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
