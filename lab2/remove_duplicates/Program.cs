bc7bba8fe8ce89aa0ed70c349fb615798de78db4
сергей оразаев <cyperman@mail.ru>
2020-03-24 15:08:23 +0300

    upload lab 2.3

---
+++ lab2/remove_duplicates/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace remove_duplicates
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if ( args.Length != 1 ) 
+            {
+                Console.WriteLine( "Incorrect number of arguments!" + "\n" +
+                    "Usage remove_duplicates.exe < input string >" );
+                Environment.Exit(1);
+            }
+
+            string str = args[ 0 ];
+
+            for ( int i = 0; i < str.Length; i++ )
+            {
+                for ( int j = i + 1; j < str.Length; j++ )
+                {
+                    if ( str[ i ] == str[ j ] )
+                    {
+                        str = str.Remove( j, 1 );
+                        j--;
+                    }
+                }
+            }
+
+            Console.WriteLine( str );
+
+        }
+    }
+}
