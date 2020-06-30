b90dd8c1c95677089401137bd8e7fb3616dffee2
orazaev-sergej <cyperman@mail.ru>
2020-06-30 21:45:42 +0300

    Update 6.1.sql

--- lab6/6.1.sql
+++ lab6/6.1.sql
@@ -1,16 +1,15 @@
 --Тактовые частоты CPU тех компьютеров, у которых объем памяти 3000 Мб. Вывод: id, cpu, memory
 
-SELECT id, cpu(MHz), memory(Mb)
+SELECT id, "cpu(MHz)", "memory(Mb)"
 FROM PC
-WHERE memory(Mb) = 3000;
+WHERE "memory(Mb)" = 3000;
 
 --Минимальный объём жесткого диска, установленного в компьютере на складе. Вывод: hdd
 
-SELECT hdd
+SELECT min("hdd(Gb)")
 FROM PC
-WHERE MIN(hdd(Gb))
              
 --Количество компьютеров с минимальным объемом жесткого диска, доступного на складе. Вывод: count, hdd
-SELECT COUNT(hdd(Gb))
+SELECT COUNT("hdd(Gb)")
 FROM PC
-WHERE MIN(hdd(Gb))
+WHERE "hdd(Gb)" = (SELECT min("hdd(Gb)") FROM PC);
