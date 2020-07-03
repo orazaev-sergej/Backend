9ffcdea8d5ca2f082c34f59b6d3503dcf4db116f
orazaev-sergej <cyperman@mail.ru>
2020-07-03 23:13:35 +0300

    Update 6.7.sql

--- lab6/6.7.sql
+++ lab6/6.7.sql
@@ -1,6 +1,23 @@
+-- 7.1
 SELECT DISTINCT users.name
 FROM
 	orders
 	JOIN users ON orders.users_id = users.users_id
 WHERE
 	status = 0
+	
+--7.2
+SELECT
+	orders_count, users_name
+FROM
+	(SELECT
+		count(users.name) as orders_count, users.name as users_name
+	FROM
+		orders
+		JOIN users ON orders.users_id = users.users_id
+	WHERE
+		status = 1
+	GROUP BY
+		users.name)
+WHERE
+	orders_count > 5
